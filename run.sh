7856f66a88bca2d0e1ce13de3e596e139d3c94de
Mormamn <mormamn@gmail.com>
2020-03-28 13:26:12 +0000

    easy way to run the server

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+env=${1:-local}
+if [ "$env" == "local" ]; then
+    echo "Running app on localhost:8000"
+    uvicorn main:app --reload --host 127.0.0.1
+elif [ "$env" == "pub" ]; then
+    echo "You might need to run the following command for this to work:"
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080"
+    echo "Running app on 0.0.0.0:80"
+    uvicorn main:app --reload --host 0.0.0.0 --port 8080
+else
+    echo "Wrong value given, must be 'pub' or 'local'"
+    echo "Input was: $1"
+fi
