7b4fc6af93587545e1df5155160eb8083f460525
Mormamn <mormamn@gmail.com>
2020-03-29 17:16:55 +0000

    Auth, s2i router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException, Depends
+from routers import s2i
+from app.auth import validate_key
 
 app = FastAPI()
+
+async def get_token_header(x_token: str = Header(...)):
+    if validate_key(x_token):
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+app.include_router(
+    s2i.router,
+    prefix="/s2i",
+    tags=["s2i"],
+    dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
 
 @app.get('/')
 def read_root():
